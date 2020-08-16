f77b184c6ac96ef9eb670a51313974caf67be90a
vinit kumar <panditvinit01@gmail.com>
2020-08-16 14:11:16 +0530

    Updates

---
+++ precache-manifest.f8129a432e49531940c78c4d3dd11920.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36a5c811a6c73d1e51aec722e5a1768e",
+    "url": "/Profile/index.html"
+  },
+  {
+    "revision": "8d71a057ffbaf774eea5",
+    "url": "/Profile/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "8d71a057ffbaf774eea5",
+    "url": "/Profile/static/js/2.de75bbe6.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Profile/static/js/2.de75bbe6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a86bfa69e4a695b3ee0",
+    "url": "/Profile/static/js/main.50508db2.chunk.js"
+  },
+  {
+    "revision": "a59d21a14984125caf9d",
+    "url": "/Profile/static/js/runtime-main.5813c363.js"
+  }
+]);
